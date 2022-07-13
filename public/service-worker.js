996a827495fbe19b59ad312efad4826a9e8dc6e4
Kate <katensullivan55@gmail.com>
2022-07-13 00:53:25 -0400

    added servioce worker install

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,3 +18,16 @@ const FILES_TO_CACHE = [
   '/js/index.js'
 ];
 
+// Install the service worker
+self.addEventListener('install', function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log('Your files were pre-cached successfully!');
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  
